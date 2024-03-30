a5d9244d3d6301ebcb791180b2efc80ad3513db9
meyab <yabsrayabsra2222@gmail.com>
2024-03-30 20:59:43 +0300

    Creating a class

---
+++ Shortest Remaning Time First(SRTF) Scheduling Algorithm.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+public class Process
+{
+    public int Id { get; set; }            
+    public int ArrivalTime { get; set; }   
+    public int BurstTime { get; set; }     
+}
