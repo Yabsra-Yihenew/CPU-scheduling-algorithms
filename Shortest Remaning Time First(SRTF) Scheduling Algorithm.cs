ed2134daf23e7f98df0f8bb263da5f482c391be4
meyab <yabsrayabsra2222@gmail.com>
2024-03-30 21:02:54 +0300

    Updating the algorithm

--- Shortest Remaning Time First(SRTF) Scheduling Algorithm.cs
+++ Shortest Remaning Time First(SRTF) Scheduling Algorithm.cs
@@ -22,3 +22,61 @@ public class SRTF
 
         ShortestRemainingTimeFirst(processes);
     }
+
+    public static void ShortestRemainingTimeFirst(List<Process> processes)
+    {
+        // Initializing a list to track the processes that are currently in execution
+        List<Process> currentProcess = new List<Process>();
+
+        // Sorting the processes based on their arrival time for proper scheduling order
+        processes.Sort((p1, p2) => p1.ArrivalTime.CompareTo(p2.ArrivalTime));
+
+        int currentTime = 0;
+
+        while (currentProcess.Count < processes.Count)
+        {
+            // Finding processes that are eligible for execution at the current time
+            List<Process> eligibleProcesses = new List<Process>();
+
+            foreach (var process in processes)
+            {
+                // Checking if the process has arrived and is not already in execution
+                if (process.ArrivalTime <= currentTime && !currentProcess.Contains(process))
+                {
+                    eligibleProcesses.Add(process);
+                }
+            }
+
+            // If there are eligible processes for execution
+            if (eligibleProcesses.Count > 0)
+            {
+                // Sorting eligible processes based on their burst time
+                eligibleProcesses.Sort((p1, p2) => p1.BurstTime.CompareTo(p2.BurstTime));
+
+                // Selecting the process with small burst time
+                Process selectedProcess = eligibleProcesses[0];
+
+                // Displaying information about the execution of the selected process
+                if (currentProcess.Count == 0 || currentProcess[0].Id != selectedProcess.Id)
+                {
+                    Console.WriteLine("Executing process " + selectedProcess.Id);
+                }
+
+                currentTime += 1;  // Assuming time quantum is 1 unit for simplicity
+                selectedProcess.BurstTime -= 1;
+
+                // If the selected process has completed its execution, add it to the list of executed processes
+                if (selectedProcess.BurstTime == 0)
+                {
+                    currentProcess.Add(selectedProcess);
+                }
+            }
+            else
+            {
+                currentTime++;  
+            }
+        }
+        
+    }
+}
+
